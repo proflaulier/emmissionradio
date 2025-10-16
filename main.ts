87390bcaa4db5871caeed64913fcb44d6e5c6c73
proflaulier <cyrille.laulier@ac-rennes.fr>
2025-10-16 09:23:23 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendValue("numCube", 4)
+    basic.showIcon(IconNames.QuarterNote)
     basic.pause(1000)
+    basic.showIcon(IconNames.EighthNote)
 })
+basic.showIcon(IconNames.QuarterNote)
 radio.setGroup(7)
